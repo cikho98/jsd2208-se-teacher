991f067c74f2943660d488a9b1f8fc59eaecfab3
TEDU <83123095@qq.com>
2022-09-29 11:45:03 +0800

    day01

--- src/main/java/file/Test3.java
+++ src/main/java/file/Test3.java
@@ -9,11 +9,18 @@ import java.io.FileFilter;
 public class Test3 {
     public static void main(String[] args) {
         File dir = new File(".");
-        FileFilter filter = new FileFilter() {
-            public boolean accept(File file) {
-                return file.getName().contains("a");
-            }
-        };
+//        FileFilter filter = new FileFilter() {
+//            public boolean accept(File file) {
+//                return file.getName().contains("a");
+//            }
+//        };
+
+        //FileFilter可以使用lambda表达式
+        //当参数只有一个时，参数列表的"()"可以忽略不写
+        FileFilter filter = f->f.getName().contains("a");
+
+
+
         File[] subs = dir.listFiles(filter);
         for(File sub : subs){
             System.out.println(sub.getName());
---
+++ src/main/java/file/Test4.java
@@ -0,0 +1,17 @@
+package file;
+
+/**
+ * 程序启动后，要求用户输入一个文件名，如果文件存在则提示
+ * 并要求用户更换，直到该文件还不存在为止，然后将这个文件
+ * 在当前目录中创建出来。
+ * 提示:
+ * Scanner
+ * 循环(while)
+ * 分支
+ * File:
+ *  createNewFile
+ *  exists
+ *
+ */
+public class Test4 {
+}
