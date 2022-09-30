4a5ccda1ac787851e96bb76669b9417174232ca2
TEDU <83123095@qq.com>
2022-09-30 16:10:48 +0800

    day01

---
+++ src/main/java/io/Test5.java
@@ -0,0 +1,22 @@
+package io;
+
+/**
+ * 设计一个User类，表示用户信息
+ * 有4个属性:username,password,nickname,age
+ * 其中age是int值，其他为String
+ *
+ * 程序启动后，要求用户在控制台上陆续输入用户名，密码，昵称，年龄
+ * 然后将该用户信息以一个User对象形式序列化到文件中。
+ * 文件名格式:用户名.obj
+ *
+ * 范传奇
+ * 123456
+ * 传奇
+ * 22
+ *
+ */
+public class Test5 {
+    public static void main(String[] args) {
+
+    }
+}
