66b094a25a0eb88ee47fb4e1dc403e01dfbbb7ac
TEDU <83123095@qq.com>
2022-09-30 15:51:41 +0800

    day01

--- src/main/java/io/Person.java
+++ src/main/java/io/Person.java
@@ -10,7 +10,13 @@ public class Person implements Serializable {
     private String name;
     private int age;
     private String gender;
-    private String[] otherInfo;
+    /*
+        当一个属性被关键字transient修饰后，那么对象序列化时就会忽略
+        该属性的值。
+        忽略不必要的属性可以达到对象瘦身的目的，在对象持久化时可以减少
+        磁盘开销。在网络传输时可以提高传输速度。
+     */
+    private transient String[] otherInfo;
 
     public Person(String name, int age, String gender, String[] otherInfo) {
         this.name = name;
