1369de2e4f68c16c4c63cc0300596f997d5a4eb2
TEDU <83123095@qq.com>
2022-09-30 17:23:49 +0800

    day01

--- src/main/java/io/Test6.java
+++ src/main/java/io/Test6.java
@@ -1,7 +1,26 @@
 package io;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 /**
  * 将当前项目目录下的所有obj文件进行反序列化，并将对应的User对象输出
  */
 public class Test6 {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File dir = new File(".");
+        File[] subs = dir.listFiles(f->f.getName().endsWith(".obj"));
+        for(File sub : subs){
+
+            FileInputStream fis = new FileInputStream(sub);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Object obj = ois.readObject();//反序列化
+            if(obj instanceof User){//判断obj指向的对象是否为User类型的实例
+                User user = (User)obj;
+                System.out.println(user);
+            }
+        }
+    }
 }
