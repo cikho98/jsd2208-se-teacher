8a6ee60d9782341013b9250c0a0763d289f61856
TEDU <83123095@qq.com>
2022-10-08 17:32:30 +0800

    day01

---
+++ src/main/java/exception/Person.java
@@ -0,0 +1,26 @@
+package exception;
+
+/**
+ * throw关键字可以主动对外抛出一个异常,通常下列情况我们会这样做:
+ * 1:程序遇到一个异常,但是该异常不应当被当前代码片段处理时可以主动对外抛出
+ * 2:程序遇到了一个满足语法但是不满足业务场景时可以主动抛出一个异常
+ */
+public class Person {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) throws Exception {
+        if(age<0||age>100){
+//           throw new RuntimeException("年龄不合法");
+            /*
+                除了RuntimeException之外的其它异常如果使用throw
+                抛出则要在当前方法上使用throws声明该异常的抛出
+             */
+            throw new Exception("年龄不合法");
+        }
+        this.age = age;
+    }
+}
---
+++ src/main/java/exception/ThrowDemo.java
@@ -0,0 +1,23 @@
+package exception;
+
+/**
+ * 异常的抛出
+ */
+public class ThrowDemo {
+    public static void main(String[] args) {
+        Person p = new Person();
+        try {
+            /*
+                当我们调用一个含有throws声明异常抛出的方法时,编译器
+                要求我们必须写处理办法,否则编译不通过.
+                处理办法有两种:
+                1:可以在当前方法上继续使用throws将异常抛出
+                2:使用try-catch主动捕获并处理该异常
+             */
+            p.setAge(10000);//满足语法,但是不满足业务
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("此人年龄"+p.getAge()+"岁");
+    }
+}
