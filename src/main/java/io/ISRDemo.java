b8e00d3976907a9b5b458fff17f5017414bfef77
TEDU <83123095@qq.com>
2022-10-08 10:01:18 +0800

    day01

--- src/main/java/io/ISRDemo.java
+++ src/main/java/io/ISRDemo.java
@@ -12,8 +12,10 @@ import java.nio.charset.StandardCharsets;
 public class ISRDemo {
     public static void main(String[] args) throws IOException {
 //        FileInputStream fis = new FileInputStream("osw.txt");
-        FileInputStream fis = new FileInputStream("./src/main/java/io/ISRDemo.java");
-        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        FileInputStream fis = new FileInputStream(
+                "./src/main/java/io/ISRDemo.java");
+        InputStreamReader isr = new InputStreamReader(
+                fis, StandardCharsets.UTF_8);
         int d;
         while((d= isr.read())!=-1){
             System.out.print((char)d);
