a238ddf660b29fa9861482029c6af73eb9d3926c
TEDU <83123095@qq.com>
2022-10-09 11:41:45 +0800

    day01

---
+++ src/main/java/socket/Server.java
@@ -0,0 +1,17 @@
+package socket;
+
+/**
+ * 聊天室服务端
+ */
+public class Server {
+    public Server(){
+
+    }
+    public void start(){
+
+    }
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+}
