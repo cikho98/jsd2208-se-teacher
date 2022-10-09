4715ea9debb5eacece2d5aec92892544fe8cb682
TEDU <83123095@qq.com>
2022-10-09 09:50:38 +0800

    day01

--- src/main/java/exception/ExceptionApiDemo.java
+++ src/main/java/exception/ExceptionApiDemo.java
@@ -10,7 +10,17 @@ public class ExceptionApiDemo {
             String line = "abc";
             System.out.println(Integer.parseInt(line));
         } catch (NumberFormatException e) {
+            System.out.println("出错了!");
+            /*
+                在控制台输出错误堆栈信息(该输出和System.out.println输出
+                顺序可能混乱,这是由于多线程原因.)
+             */
             e.printStackTrace();
+            /*
+                获取错误消息,一般用于记录日志或提示给用户使用
+             */
+            String message = e.getMessage();//获取错误消息
+            System.out.println(message);
         }
         System.out.println("程序结束了");
     }
