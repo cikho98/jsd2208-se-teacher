66e4a3b3826bf7e53d5eddc46237fbdac56048bd
TEDU <83123095@qq.com>
2022-10-09 17:22:01 +0800

    day01

--- src/main/java/thread/ThreadDemo.java
+++ src/main/java/thread/ThreadDemo.java
@@ -8,6 +8,9 @@ public class ThreadDemo {
     public static void main(String[] args) {
         Thread t1 = new MyThread1();
         Thread t2 = new MyThread2();
+        /*
+            需要注意,启动线程要调用线程的start方法,而不是直接调用run方法!
+         */
         t1.start();
         t2.start();
 
